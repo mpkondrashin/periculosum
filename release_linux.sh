66aef62303e0e0123a325c3ec66116df44cc88f4
Mikhail Kondrashin <mkondrashin@gmail.com>
2022-08-20 11:19:23 +0300

    change release file to zip

---
+++ release_linux.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#
+# Copyright (C) 2022 Michael Kondrashin — All Rights Reserved
+# You may use, distribute and modify this code under the
+# terms of the MIT license.
+# You should have received a copy of the MIT license with
+# this file. If not, please write to: mkondrashin@gmail.com
+#
+# Lates version of this file can be found at following URL:
+# https://github.com/mpkondrashin/periculosum
+#
+# release_linux.sh - pack release files to one zip file
+#
+
+set -eu
+export PS4='> '
+set -x
+
+cd build
+
+rm -rf periculosum
+mkdir periculosum
+cp ../periculosum periculosum
+cp ../checker periculosum
+cp ../LICENSE periculosum
+cp ../README.md periculosum
+
+zip -r ../periculosum_linux64.zip periculosum
